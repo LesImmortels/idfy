0318010cff0ae35df32c0b19f9d85d6ec16e98d4
adam <adamalany@gmail.com>
2022-11-19 19:03:15 +0100

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,58 @@
-function Navbar() {
-    return (
-        <div className="sidebar">
-            <p>Navbar</p>
-        </div>
-    );
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'SCAN ART', href: 'search', current: true },
+  { name: 'NEW GENERATIONS', href: 'browse', current: false },
+  { name: 'PRICING', href: 'pricing', current: false },
+  { name: 'PRODUCTS', href: 'products', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Navbar;
+export default function Navbar() {
+  return (
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-16 lg:px-16">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-12 w-auto"
+                    src={process.env.PUBLIC_URL + '/wallet.png'}
+                    alt="Your Company"
+                  />
+                </div>
+
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'outline outline-1 outline-red-600 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-xs font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
+  )
+}
