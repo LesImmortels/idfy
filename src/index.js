935ff4166f657bfcf6ebaec326a9c751fc57bfa2
adam <adamalany@gmail.com>
2022-11-19 14:06:49 +0100

    added basic routing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from "./screens/home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
