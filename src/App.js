1385239f76a1e1452f38139db81db70eac6dc00d
noe.urso-baiardo <noe.urso-baiardo@epita.fr>
2022-11-19 17:43:51 +0100

    finally the end of the qr code generation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import { useState } from "react";
-import QRCodeStyling from "qr-code-styling";
+import QR from './components/qr';
 
+/*
 
 const { Configuration, OpenAIApi } = require("openai");
 
@@ -11,25 +12,11 @@ const configuration = new Configuration({
   apiKey: process.env.REACT_APP_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);*/
 
 function App() {
   return (
-    <div className="App">
-      {
-        imageUrl
-          ? <img src={imageUrl} className="image" alt="ai thing" />
-          : <img src={logo} className="image" alt="logo" />
-      }
-      <div id="canvas"></div>
-      <p>Generate a unique image using DALL·E</p>
-      <p>What do you want to see?</p>
-      <input
-        placeholder='A sunset on the Sydney Opera House'
-        onChange={(e) => setUserPrompt(e.target.value)}
-      />
-      <button onClick={() => generateImage()}>Generate</button>
-    </div>
+    <QR></QR>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import QR from './components/qr';
 import './index.css';
 import Home from "./screens/home"
 
@@ -7,5 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home/>
+    <QR></QR>
   </React.StrictMode>
 );
